863c60af290bf7bc48f6f1d0c95b28462256cf46
System Administrator <root@shefas-MacBook-Pro.local>
2020-03-15 22:51:46 -0400

    adding .travis.yml and deploy.sh files2

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '3.90.31.176',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: 'is-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
